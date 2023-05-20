bb18c46a93cb3168767a8ef35df377ec4d2429e0
Mmaryna68 <maryov688@gmail.com>
2023-05-20 12:17:28 +0100

    add comments js

--- week 21/transport.js
+++ week 21/transport.js
@@ -1,3 +1,4 @@
+// Определение базового класса Transport
 class Transport {
   constructor(type, price, brand) {
     this.type = type;
@@ -5,37 +6,44 @@ class Transport {
     this.brand = brand;
   }
 
+  // Метод для получения информации о транспорте
   getInfo() {
     return `Type: ${this.type}, Brand: ${this.brand}`;
   }
 
+  // Метод для получения цены транспорта
   getPrice() {
     return `Price: $${this.price}`;
   }
 }
 
+// Класс Car, наследующийся от Transport
 class Car extends Transport {
   constructor(type, price, brand, doors) {
     super(type, price, brand);
     this.doors = doors;
   }
 
+  // Метод для получения количества дверей
   getDoorsCount() {
     return `Doors: ${this.doors}`;
   }
 }
 
+// Класс Bike, наследующийся от Transport
 class Bike extends Transport {
   constructor(type, price, brand, maxSpeed) {
     super(type, price, brand);
     this.maxSpeed = maxSpeed;
   }
 
+  // Метод для получения максимальной скорости
   getMaxSpeed() {
     return `Max Speed: ${this.maxSpeed} km/h`;
   }
 }
 
+// Массив данных о транспорте
 const data = [
   {
     id: 1,
@@ -74,41 +82,54 @@ const data = [
   },
 ];
 
+// Получаем ссылку на контейнер для транспортных средств
 const vehicleContainer = document.getElementById("vehicle-container");
 
+// Перебор по каждому элементу массива данных и создание карточек
 data.forEach((item) => {
   let vehicle;
 
+  // Проверяем тип транспортного средства
   if (item.type === "car") {
+    // Создаем экземпляр класса Car для автомобилей
     vehicle = new Car(item.type, item.price, item.brand, item.doors);
   } else if (item.type === "bike") {
+    // Создаем экземпляр класса Bike для мотоциклов
     vehicle = new Bike(item.type, item.price, item.brand, item.maxSpeed);
   }
 
+  // Создаем элемент-контейнер для карточки транспортного средства
   const card = document.createElement("div");
   card.className = "vehicle-card";
 
+  // Создаем элемент img для отображения изображения
   const image = document.createElement("img");
   image.src = item.image;
   card.appendChild(image);
 
+  // Создаем элемент p для отображения информации о транспортном средстве
   const info = document.createElement("p");
   info.textContent = vehicle.getInfo();
   card.appendChild(info);
 
+  // Создаем элемент p для отображения цены транспортного средства
   const price = document.createElement("p");
   price.textContent = vehicle.getPrice();
   card.appendChild(price);
 
+  // Проверяем тип транспортного средства
   if (vehicle instanceof Car) {
+    // Для автомобилей добавляем элемент p для отображения количества дверей
     const doorsCount = document.createElement("p");
     doorsCount.textContent = vehicle.getDoorsCount();
     card.appendChild(doorsCount);
   } else if (vehicle instanceof Bike) {
+    // Для велосипедов добавляем элемент p для отображения максимальной скорости
     const maxSpeed = document.createElement("p");
     maxSpeed.textContent = vehicle.getMaxSpeed();
     card.appendChild(maxSpeed);
   }
 
+  // Добавляем карточку транспортного средства в контейнер
   vehicleContainer.appendChild(card);
 });
